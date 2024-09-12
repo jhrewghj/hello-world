a81a18eedb638edc99cbdcd36346656277b1c459
jhrewghj <cyin@tmsacademy.org>
2024-09-12 07:55:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-# usrnme:hi,psswd:abc123
-
-
-
-from flask import Flask, request, jsonify, render_template
+import sys
+from flask import Flask, jsonify, request
 from flask_cors import CORS  # Import CORS
-
+import hashing as h
 app = Flask(__name__)
-
-# Allow requests from all origins (replace with specific origins for production)
 CORS(app)
-
-# This is where you define your routes
+#put hashing print over here
 @app.route('/submit', methods=['POST'])
+
 def submit_data():
     data = request.get_json()
+    u_and_p = [data['name'], data['message']]
+    c = h.hashedinfo(u_and_p[0], u_and_p[1])
     print("Received data:", data)
     response = {"message": "some data"}
     print(data)
-    if data == {'name': 'username=hi', 'message': '1'}:
-        return jsonify({"message": "https://tinf0il.tech"}), 200
+    d = ["a226abda121d6a22bbb9d858a94c9ce1","55be20677b1caac1b4d53058800b06ef"]
+
+    if c == d:
+        return jsonify({"message": "https://h"}), 200
     else:
         return jsonify({"message": "do normal"}), 200
 
 # This block is for running your Flask app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
