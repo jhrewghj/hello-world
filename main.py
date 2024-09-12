8b5b81fb4b64d4ab332c145a934e009030c4ff98
jhrewghj <cyin@tmsacademy.org>
2024-09-12 15:02:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ def submit_data():
     d = ["a226abda121d6a22bbb9d858a94c9ce1","55be20677b1caac1b4d53058800b06ef"]
     e = ['e2884f8636953a76dc7833ec63f081f3', 'eeaf08b96e493abb74af595db835f36c'] #ilovedogs, 1oi6bm
     f = ['3ed6d0fac7dcd33714fed5c9d3b0076f', '38659adc997e4898eb812b2de57e5297'] #tinf0il, tinfoil
+    g = ['7d128fea364420e74c62bff0fa777a36', 'afc5d26d56f3993ecb8266d96a50c4fd'] #spacestation123, b98z'
 
     if c == d:
         return jsonify({"message": "https://surfdoge.pro"}), 200
     elif c == e:
         return jsonify({"message": "https://surfdoge.pro"}), 200
     elif c == f:
+        return jsonify({"message": "https://tinf0il.tech"}), 200
+    elif c == g:
         return jsonify({"message": "https://tinf0il.tech"}), 200
     else:
         return jsonify({"message": "do normal"}), 200
